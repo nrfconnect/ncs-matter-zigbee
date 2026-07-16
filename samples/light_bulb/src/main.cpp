20dddb4148bd7c5a74b5c47dbab861eb0aee83bc
Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>
2026-07-16 10:30:49 +0200

    Added base structure for compilation testing
    Created minimum number of files to run samples compilation
    in CI integration:
    * empty samples with sample.ymls
    * root Kconfig
    * root Cmake
    * sysbuild cmake
    
    Signed-off-by: Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>

---
+++ samples/light_bulb/src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/** @file
+ *
+ * @brief Matter + Zigbee light bulb entry point.
+ */
+
+int main(void)
+{
+	return 0;
+}
